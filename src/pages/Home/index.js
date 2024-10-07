95e77fe4abb5ae348a8795aaa3ff259e5cf668f9
Calvin0411 <ramjeeawoncalvin@gmail.com>
2024-10-07 16:51:32 +0200

    Ancre du header
    J'ai terminé de finaliser les ancres de mon header

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -22,7 +22,7 @@ const Page = () => {
       <Menu />
     </header>
     <main>
-      <section className="SliderContainer">
+      <section className="SliderContainer" id="nos-services">
         <Slider />
       </section>
       <section className="ServicesContainer">
@@ -54,11 +54,11 @@ const Page = () => {
           </ServiceCard>
         </div>
       </section>
-      <section className="EventsContainer">
+      <section className="EventsContainer" id="nos-realisations">
         <h2 className="Title">Nos réalisations</h2>
         <EventList />
       </section>
-      <section className="PeoplesContainer">
+      <section className="PeoplesContainer" id="notre-equipe">
         <h2 className="Title">Notre équipe</h2>
         <p>Une équipe d’experts dédiés à l’ogranisation de vos événements</p>
         <div className="ListContainer">
